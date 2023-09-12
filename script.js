3b941832d403d1cebbbe58fbe7b08100a0c7b0f0
2100031349 <110711963+2100031349@users.noreply.github.com>
2023-09-12 22:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function convertTemperature() {
+    
+    const inputTemperature = parseFloat(document.getElementById("inputTemperature").value);
+    const unit = document.getElementById("unit").value;
+
+    
+    let result;
+    if (unit === "celsius") {
+        result = (inputTemperature * 9/5) + 32; // Celsius to Fahrenheit
+    } else {
+        result = (inputTemperature - 32) * 5/9; // Fahrenheit to Celsius
+    }
+
+    
+    document.getElementById("resultTemperature").textContent = `Result: ${result.toFixed(2)} ${unit === "celsius" ? "Fahrenheit" : "Celsius"}`;
+}
 
